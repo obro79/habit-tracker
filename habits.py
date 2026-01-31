219e46b5a32797665547b03a92a9cdbaac6d1b54
owenfisher <owenfisher46@gmail.com>
2026-01-30 16:14:41 -0800

    MVP Done

--- habits.py
+++ habits.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from datetime import date, timedelta
 
 file_path = "data/habits.json"
 
@@ -14,6 +15,7 @@ def add_habit(args):
     new_entry = {
         "Name": args.name,
         "Streak": 0,
+        "Last Checked": date.today().isoformat(),
     }
 
     try:
@@ -45,11 +47,55 @@ def list_habits(args):
 
 
 def check_habit(args):
-    print(f"Checking off habit: {args.name}")
+    """Check off a habit for today"""
+
+    try:
+        with open(file_path, "r") as json_file:
+            data = json.load(json_file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {}
+    if args.name not in data:
+        raise ValueError(f"Habit '{args.name}' does not exist.")
+
+    habit = data[args.name]
+    last_checked = date.fromisoformat(habit["Last Checked"])
+    today = date.today()
+    if last_checked == today:
+        print(f"Habit '{args.name}' already checked for today.")
+        return
+    elif last_checked == today - timedelta(days=1):
+        habit["Streak"] += 1
+    else:
+        habit["Streak"] = 1
+
+    habit["Last Checked"] = today.isoformat()
+
+    with open(file_path, "w") as json_file:
+        json.dump(data, json_file, indent=4)
+    print(f"Habit '{args.name}' checked successfully.")
 
 
 def remove_habit(args):
-    print(f"Removing habit: {args.name}")
+    """Removes a habit from the Json file
+
+    Args:
+        args (_type_): _command line arguments
+    """
+    try:
+        with open(file_path, mode="r") as json_file:
+            data = json.load(json_file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {}
+
+    if args.name in data:
+        del data[args.name]
+    else:
+        print(f"{args.name} was already removed")
+
+    with open(file_path, mode="w") as json_file:
+        json.dump(data, json_file, indent=4)
+
+    print("Habit was removed successfully")
 
 
 def main():
@@ -67,6 +113,10 @@ def main():
 
     list_parser = subparsers.add_parser("list", help="List all habits")
     list_parser.set_defaults(func=list_habits)
+
+    remove_parser = subparsers.add_parser("remove", help="Remove a habits")
+    remove_parser.add_argument("name", type=str, help="Name of the habit")
+    remove_parser.set_defaults(func=remove_habit)
 
     args = parser.parse_args()
     args.func(args)
